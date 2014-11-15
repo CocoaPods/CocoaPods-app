e1a005f630dc847d06a50437fd316b188600783d
Eloy Durán <eloy.de.enige@gmail.com>
2014-11-15 17:02:54 +0100

    Import app skeleton.

---
+++ app/CocoaPods/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  CocoaPods
+//
+//  Created by Eloy Durán on 15/11/14.
+//  Copyright (c) 2014 CocoaPods. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+  // Insert code here to tear down your application
+}
+
+@end
---
+++ app/CocoaPods/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CocoaPods
+//
+//  Created by Eloy Durán on 15/11/14.
+//  Copyright (c) 2014 CocoaPods. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
