3c16fbee6e84f26225e393e9285211d9582f44e6
Eloy Durán <eloy.de.enige@gmail.com>
2014-11-15 17:12:39 +0100

    Add `pod` target.

---
+++ app/pod/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pod
+//
+//  Created by Eloy Durán on 15/11/14.
+//  Copyright (c) 2014 CocoaPods. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
