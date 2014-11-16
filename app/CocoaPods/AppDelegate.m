5c6a10ed628631e63afbdf4c13cd19b4ebc23d82
Eloy Durán <eloy.de.enige@gmail.com>
2014-11-16 18:02:19 +0100

    Get rid of ridiculous comments.

--- app/CocoaPods/AppDelegate.m
+++ app/CocoaPods/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  CocoaPods
-//
-//  Created by Eloy Durán on 15/11/14.
-//  Copyright (c) 2014 CocoaPods. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
