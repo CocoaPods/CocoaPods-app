dcf28a5d80b6d39c79ff3d5175b8c0194a25db03
Eloy Durán <eloy.de.enige@gmail.com>
2015-04-05 16:06:00 +0200

    [app] Update Pods with 0.36.3

--- app/Pods/Target Support Files/Pods/Pods-resources.sh
+++ app/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -6,6 +6,8 @@ mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
 > "$RESOURCES_TO_COPY"
 
+XCASSET_FILES=""
+
 install_resource()
 {
   case $1 in
@@ -36,6 +38,7 @@ install_resource()
       xcrun mapc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcmappingmodel`.cdm"
       ;;
     *.xcassets)
+      XCASSET_FILES="$XCASSET_FILES '${PODS_ROOT}/$1'"
       ;;
     /*)
       echo "$1"
@@ -47,22 +50,34 @@ install_resource()
       ;;
   esac
 }
-          install_resource "Fragaria/SMLCommandResult.xib"
-                    install_resource "Fragaria/SMLDetab.xib"
-                    install_resource "Fragaria/SMLEntab.xib"
-                    install_resource "Fragaria/SMLGoToLine.xib"
-                    install_resource "Fragaria/SMLOpenPanelAccessoryView.xib"
-                    install_resource "Fragaria/SMLRegularExpressionHelp.xib"
-                    
-                    
-          
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "Fragaria/SMLCommandResult.xib"
+  install_resource "Fragaria/SMLDetab.xib"
+  install_resource "Fragaria/SMLEntab.xib"
+  install_resource "Fragaria/SMLGoToLine.xib"
+  install_resource "Fragaria/SMLOpenPanelAccessoryView.xib"
+  install_resource "Fragaria/SMLRegularExpressionHelp.xib"
+  
+  
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "Fragaria/SMLCommandResult.xib"
+  install_resource "Fragaria/SMLDetab.xib"
+  install_resource "Fragaria/SMLEntab.xib"
+  install_resource "Fragaria/SMLGoToLine.xib"
+  install_resource "Fragaria/SMLOpenPanelAccessoryView.xib"
+  install_resource "Fragaria/SMLRegularExpressionHelp.xib"
+  install_resource "Fragaria/Syntax Definitions"
+  install_resource "Fragaria/SyntaxDefinitions.plist"
+fi
+
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
   rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${INSTALL_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 fi
 rm -f "$RESOURCES_TO_COPY"
 
-if [[ -n "${WRAPPER_EXTENSION}" ]] && [ "`xcrun --find actool`" ] && [ `find . -name '*.xcassets' | wc -l` -ne 0 ]
+if [[ -n "${WRAPPER_EXTENSION}" ]] && [ "`xcrun --find actool`" ] && [ -n "$XCASSET_FILES" ]
 then
   case "${TARGETED_DEVICE_FAMILY}" in
     1,2)
@@ -78,5 +93,6 @@ then
       TARGET_DEVICE_ARGS="--target-device mac"
       ;;
   esac
-  find "${PWD}" -name "*.xcassets" -print0 | xargs -0 actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" ${TARGET_DEVICE_ARGS} --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+  while read line; do XCASSET_FILES="$XCASSET_FILES '$line'"; done <<<$(find "$PWD" -name "*.xcassets" | egrep -v "^$PODS_ROOT")
+  echo $XCASSET_FILES | xargs actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" ${TARGET_DEVICE_ARGS} --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 fi
