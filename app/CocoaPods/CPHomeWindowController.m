ebf992218904064bad9e8cf89151d7b39a22e1d2
William Kent <wjk@users.noreply.github.com>
2015-09-12 17:34:03 -0400

    Add CPHomeWindowDocumentEntry class

--- app/CocoaPods/CPHomeWindowController.m
+++ app/CocoaPods/CPHomeWindowController.m
@@ -8,6 +8,31 @@
 
 #import "CPHomeWindowController.h"
 
+@interface CPHomeWindowDocumentEntry : NSObject <NSCopying>
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSURL *podfileURL;
+@property (nonatomic, copy) NSImage *image;
+@property (nonatomic, copy) NSString *folderPath;
+@end
+
+@implementation CPHomeWindowDocumentEntry
+
+- (instancetype)copyWithZone:(NSZone *)zone {
+  CPHomeWindowDocumentEntry *copy = [[[self class] allocWithZone:zone] init];
+  if (copy) {
+    [copy setName:[self.name copyWithZone:zone]];
+    [copy setPodfileURL:[self.podfileURL copyWithZone:zone]];
+    [copy setImage:[self.image copyWithZone:zone]];
+    [copy setFolderPath:[self.folderPath copyWithZone:zone]];
+  }
+
+  return copy;
+}
+
+@end
+
+#pragma mark -
+
 @interface CPHomeWindowController ()
 
 @end
