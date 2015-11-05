ff40a905606eefb70487eeb1b69ece07c2321e9e
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-11-04 17:33:44 -0800

    Autoscroll the pod output console (Closes #82)

--- app/CocoaPods/CPUserProject.m
+++ app/CocoaPods/CPUserProject.m
@@ -258,6 +258,11 @@ ANSIUnescapeString(NSString *input) {
 
 - (void)appendTaskOutput:(NSString *)rawOutput;
 {
+  // Determine if we're at the tail of the output log (and should scroll) before we append more to it.
+  CGRect visibleRect = self.progressOutputView.enclosingScrollView.documentVisibleRect;
+  CGFloat maxContentOffset = self.progressOutputView.bounds.size.height - visibleRect.size.height;
+  BOOL scrolledToBottom = visibleRect.origin.y == maxContentOffset;
+
   NSAttributedString *attributedOutput = ANSIUnescapeString(rawOutput);
   if (self.taskOutput) {
     NSMutableAttributedString *existingOutput = [self.taskOutput mutableCopy];
@@ -265,6 +270,11 @@ ANSIUnescapeString(NSString *input) {
     self.taskOutput = [existingOutput copy];
   } else {
     self.taskOutput = attributedOutput;
+  }
+
+  // Keep the text view at the bottom if it was previously.
+  if (scrolledToBottom) {
+    [self.progressOutputView scrollToEndOfDocument:self];
   }
 }
 
