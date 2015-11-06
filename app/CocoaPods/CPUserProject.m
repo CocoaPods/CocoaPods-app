a6055fc019dc7887787feb526abdad191202a396
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-11-05 18:23:33 -0800

    Move CLI task logic into its own object.

--- app/CocoaPods/CPUserProject.m
+++ app/CocoaPods/CPUserProject.m
@@ -6,6 +6,7 @@
 #import <objc/runtime.h>
 
 #import "CPANSIEscapeHelper.h" 
+#import "CPCLITask.h"
 
 // Hack SMLTextView to also consider the leading colon when completing words, which are all the
 // symbols that we support.
@@ -41,7 +42,7 @@ enum {
 typedef NSInteger NSModalResponse;
 #endif
 
-@interface CPUserProject () <NSTextViewDelegate>
+@interface CPUserProject () <CPCLITaskDelegate, NSTextViewDelegate>
 
 // Such sin.
 // TODO: Add real custom window controllers.
@@ -50,8 +51,7 @@ typedef NSInteger NSModalResponse;
 
 @property (strong) IBOutlet MGSFragariaView *editor;
 @property (strong) NSString *contents;
-@property (strong) NSTask *task;
-@property (strong) NSAttributedString *taskOutput;
+@property (strong) CPCLITask *task;
 @end
 
 @implementation CPUserProject
@@ -110,12 +110,12 @@ typedef NSInteger NSModalResponse;
 
 + (NSSet *)keyPathsForValuesAffectingProgressButtonTitle;
 {
-  return [NSSet setWithObject:@"task"];
+  return [NSSet setWithObject:@"task.progress.fractionCompleted"];
 }
 
 - (NSString *)progressButtonTitle;
 {
-  return self.task == nil ? @"Done" : @"Cancel";
+  return self.task.progress.fractionCompleted == 1.0f ? @"Done" : @"Cancel";
 }
 
 - (void)presentProgressSheet;
@@ -123,10 +123,11 @@ typedef NSInteger NSModalResponse;
   NSWindowController *controller = self.windowControllers[0];
   [controller.window beginSheet:self.progressWindow completionHandler:nil];
 }
+
 - (IBAction)dismissProgressSheet:(id)sender;
 {
-  if (self.task.isRunning) {
-    [self.task interrupt];
+  if (self.task.running) {
+    [self.task cancel];
   }
   
   [NSApp endSheet:self.progressWindow returnCode:NSModalResponseStop];
@@ -141,7 +142,8 @@ typedef NSInteger NSModalResponse;
 
 - (void)resetSheet;
 {
-  self.taskOutput = nil;
+  self.task = nil;
+  self.progressOutputView.string = @"";
 }
 
 #pragma mark - Command execution
@@ -156,126 +158,27 @@ typedef NSInteger NSModalResponse;
   [self executeTaskWithCommand:@"install"];
 }
 
-- (void)executeTaskWithCommand:(NSString *)command;
+- (void)executeTaskWithCommand:(NSString *)command
 {
-  if (self.isDocumentEdited) {
-    [self saveDocument:nil];
-  }
-
-  NSDictionary *environment = @{
-    @"HOME": NSHomeDirectory(),
-    @"LANG": @"en_GB.UTF-8",
-    @"TERM": @"xterm-256color"
-  };
-
-  NSString *workingDirectory = [[self.fileURL URLByDeletingLastPathComponent] path];
-  NSString *launchPath = @"/bin/sh";
-  NSString *envBundleScript = [[NSBundle mainBundle] pathForResource:@"bundle-env"
-                                                              ofType:nil
-                                                        inDirectory:@"bundle/bin"];
-
-  NSArray *arguments = @[envBundleScript, @"pod", command, @"--ansi"];
-  if ([[NSUserDefaults standardUserDefaults] boolForKey:@"CPShowVerboseCommandOutput"]) {
-    arguments = [arguments arrayByAddingObject:@"--verbose"];
-  }
-
-#ifdef DEBUG
-  NSString *args = [arguments componentsJoinedByString:@" "];
-  NSLog(@"$ cd '%@' && env HOME='%@' LANG='%@' TERM='%@' %@ %@", workingDirectory,
-                                                                 environment[@"HOME"],
-                                                                 environment[@"LANG"],
-                                                                 environment[@"TERM"],
-                                                                 launchPath,
-                                                                 args);
-#endif
-
-  self.task = [NSTask new];
-  self.task.launchPath = launchPath;
-  self.task.arguments = arguments;
-  self.task.environment = environment;
-  self.task.currentDirectoryPath = workingDirectory;
-
-  NSPipe *outputPipe = [NSPipe pipe];
-  self.task.standardOutput = outputPipe;
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(outputAvailable:)
-                                               name:NSFileHandleDataAvailableNotification
-                                             object:[outputPipe fileHandleForReading]];
-  [[outputPipe fileHandleForReading] waitForDataInBackgroundAndNotify];
-
-  NSPipe *errorPipe = [NSPipe pipe];
-  self.task.standardError = errorPipe;
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(outputAvailable:)
-                                               name:NSFileHandleDataAvailableNotification
-                                             object:[errorPipe fileHandleForReading]];
-  [[errorPipe fileHandleForReading] waitForDataInBackgroundAndNotify];
-
-  [self.task launch];
+  self.task = [[CPCLITask alloc] initWithUserProject:self
+                                             command:command
+                                            delegate:self
+                                    qualityOfService:NSQualityOfServiceUtility];
+  [self.task run];
+  
   [self presentProgressSheet];
 }
 
-#pragma mark - Command output
+#pragma mark - CPCLITaskDelegate
 
-// Not doing anything differently with stdout vs stderr atm.
-- (void)outputAvailable:(NSNotification *)notification;
-{
-  NSFileHandle *fileHandle = notification.object;
-  NSData *data = fileHandle.availableData;
-
-  if (data.length > 0) {
-    NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    [self appendTaskOutput:output];
-  }
-
-  if (self.task.isRunning) {
-    [fileHandle waitForDataInBackgroundAndNotify];
-  } else {
-    [self taskDidFinish];
-  }
-}
-
-- (void)taskDidFinish;
-{
-  [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                  name:NSFileHandleDataAvailableNotification
-                                                object:nil];
-
-  NSUserNotification *completionNotification = [[NSUserNotification alloc] init];
-  completionNotification.title = NSLocalizedString(@"WORKSPACE_GENERATED_NOTIFICATION_TITLE", nil);
-  completionNotification.subtitle = [[self.fileURL relativePath] stringByAbbreviatingWithTildeInPath];
-  [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:completionNotification];
-  
-  // Setting to `nil` signals through bindings that task has finished.
-  self.task = nil;
-}
-
-static NSAttributedString *
-ANSIUnescapeString(NSString *input) {
-  static CPANSIEscapeHelper *cpANSIEscapeHelper = nil;
-  static dispatch_once_t onceToken = 0;
-  dispatch_once(&onceToken, ^{
-    // Re-use the font that the text editor is configured to use.
-    cpANSIEscapeHelper = [[CPANSIEscapeHelper alloc] init];
-  });
-  return [cpANSIEscapeHelper attributedStringWithANSIEscapedString:input];
-}
-
-- (void)appendTaskOutput:(NSString *)rawOutput;
+- (void)task:(CPCLITask *)task didUpdateOutputContents:(NSAttributedString *)updatedOutput
 {
   // Determine if we're at the tail of the output log (and should scroll) before we append more to it.
   CGRect visibleRect = self.progressOutputView.enclosingScrollView.documentVisibleRect;
   CGFloat maxContentOffset = self.progressOutputView.bounds.size.height - visibleRect.size.height;
   BOOL scrolledToBottom = visibleRect.origin.y == maxContentOffset;
 
-  NSAttributedString *attributedOutput = ANSIUnescapeString(rawOutput);
-  if (self.taskOutput) {
-    NSMutableAttributedString *existingOutput = [self.taskOutput mutableCopy];
-    [existingOutput appendAttributedString:attributedOutput];
-    self.taskOutput = [existingOutput copy];
-  } else {
-    self.taskOutput = attributedOutput;
-  }
+  [self.progressOutputView.textStorage setAttributedString:updatedOutput];
 
   // Keep the text view at the bottom if it was previously, otherwise restore the previous position.
   if (scrolledToBottom) {
