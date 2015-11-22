69bb3002f9471eada361492ecf3a91699f9419cf
Eloy Durán <eloy.de.enige@gmail.com>
2015-11-23 00:49:37 +0100

    [App/Ruby] Get RubyGems to load.

--- app/CocoaPods/Ruby Integration/RBObject+CocoaPods.rb
+++ app/CocoaPods/Ruby Integration/RBObject+CocoaPods.rb
@@ -1,6 +1,17 @@
 # This is required for Foundation classes to be known to this side of the bridge at all.
 require 'osx/objc/foundation'
 
+app_bundle = OSX::NSBundle.mainBundle
+bundle_path = File.join(app_bundle.resourcePath, 'bundle')
+rubycocoa_framework = File.join(app_bundle.privateFrameworksPath, 'RubyCocoa.framework/Versions/A')
+
+# Fix all load paths to point to the bundled Ruby.
+$LOAD_PATH.map! do |path|
+  path.sub(rubycocoa_framework, bundle_path)
+end
+
+require 'rubygems'
+
 class SomeRubyClass
   def some_ruby_method(array, flag)
     { :key => "#{array.first[42]}, you are #{flag ? 'now' : 'not'} rocking with the best!" }
---
+++ app/CocoaPods/Ruby Integration/thread.rb
@@ -0,0 +1,2 @@
+# This stdlib has moved to Core, but RubyGems and ActiveSupport still try to load it,
+# so this shim is just to fix that.
