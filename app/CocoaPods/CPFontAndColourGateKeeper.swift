3fa8ecc8b23782e7f4e2c11b1e34c03f11bdbb79
Orta Therox <orta.therox@gmail.com>
2015-11-27 15:56:57 -0500

    Add a brown tinge to the background

---
+++ app/CocoaPods/CPFontAndColourGateKeeper.swift
@@ -0,0 +1,13 @@
+//
+//  CPFontAndColourGateKeeper.swift
+//  CocoaPods
+//
+//  Created by Orta on 11/27/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+class CPFontAndColourGateKeeper: NSObject {
+
+}
