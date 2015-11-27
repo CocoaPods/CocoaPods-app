6fee77657555f3adba88b1d129f0c1419be4f4cf
Eloy Durán <eloy.de.enige@gmail.com>
2015-11-27 23:56:43 +0100

    [App/Ruby] Revert monkey-patch for Swift issue, use RC branch for now.
    https://github.com/alloy/rubycocoa/tree/cocoapods-app-with-swift

--- app/CocoaPods/Ruby Integration/RBObject+CocoaPods.rb
+++ app/CocoaPods/Ruby Integration/RBObject+CocoaPods.rb
@@ -1,21 +1,3 @@
-require 'osx/objc/oc_import'
-
-module OSX
-  def ns_import_all
-    OSX.objc_classnames do |klassname|
-      # ignore private classes, such as starting with "_".
-      if /\A[A-Z]/ =~ klassname && !klassname.include?('.')
-        if OSX.const_defined?(klassname)
-          next
-        end
-        OSX.ns_import(klassname)
-      end
-    end
-    return nil
-  end
-  module_function :ns_import_all
-end
-
 # This is required for Foundation classes to be known to this side of the bridge at all.
 require 'osx/objc/foundation'
 
