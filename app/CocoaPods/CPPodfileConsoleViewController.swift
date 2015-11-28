5067e86be2381a919134e23b383d921b39f8f2e1
Daniel Wickes <djw385@nyu.edu>
2015-11-28 16:00:20 -0500

    Update CPPodfileConsoleViewController.swift

--- app/CocoaPods/CPPodfileConsoleViewController.swift
+++ app/CocoaPods/CPPodfileConsoleViewController.swift
@@ -3,7 +3,7 @@ import Cocoa
 class CPPodfileConsoleViewController: NSViewController, NSTextViewDelegate {
   @IBOutlet var textView: NSTextView!
 
-  dynamic var editable = false
+  dynamic var editable = false // the textview in the storyboard is bound to this
     
   override func viewDidLoad() {
     super.viewDidLoad()
