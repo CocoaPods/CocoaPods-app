a605ff7ca65df04a5bc9e7e0d737b11ae73b8b3f
Orta Therox <orta.therox@gmail.com>
2016-01-06 12:28:51 +0000

    [rakefile] Look into deployment on 1.0.0.b.2

---
+++ test/Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Orta Therox on 06/01/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Orta Therox on 06/01/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
