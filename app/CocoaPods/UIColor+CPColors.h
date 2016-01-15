a0a005249b2a8554accf971208e3926332aabdc3
Maxim Cramer <mennenia@gmail.com>
2016-01-15 13:33:34 +0000

    added category for colours

---
+++ app/CocoaPods/UIColor+CPColors.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+CPColors.h
+//  CocoaPods
+//
+//  Created by Maxim Cramer on 15/01/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+
+
+@interface UIColor (CPColors)
+
+
+
+@end
