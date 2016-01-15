381f8e875b1f95b61f5305ce386a827ed833580d
Maxim Cramer <mennenia@gmail.com>
2016-01-15 13:47:02 +0000

    some colors were added

--- app/CocoaPods/NSColor+CPColors.h
+++ app/CocoaPods/NSColor+CPColors.h
@@ -5,13 +5,22 @@
 //  Created by Maxim Cramer on 15/01/2016.
 //  Copyright © 2016 CocoaPods. All rights reserved.
 //
+#import <Cocoa/Cocoa.h>
 
+@interface NSColor(CPColors)
 
+// *** Default color palette ***
 
-@interface NSColor (CPColors)
+- (NSColor *)CPDarkRedColor;
+- (NSColor *)CPBrightRedColor;
+- (NSColor *)CPLightGrayColor;
+- (NSColor *)CPGrayColor;
+- (NSColor *)CPDarkColor;
 
-- (NSColor *)CPRedColor;
+// *** ANSI color palette ***
 
+- (NSColor *)ansiMutedWhite;
+- (NSColor *)ansiBrightWhite;
 
 
 @end
