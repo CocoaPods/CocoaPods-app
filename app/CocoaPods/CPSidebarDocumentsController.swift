81bcffcc085965ad9978ade1447cc56d2ab40875
Orta Therox <orta.therox@gmail.com>
2016-01-16 14:30:47 +0000

    Allow jumping between the recent / spotlight

--- app/CocoaPods/CPSidebarDocumentsController.swift
+++ app/CocoaPods/CPSidebarDocumentsController.swift
@@ -3,16 +3,35 @@ import Cocoa
 class CPSidebarDocumentsController: NSObject {
 
   // This changes between Recent and Spotlight docs
-  var currentSidebarItems = [CPHomeWindowDocumentEntry]()
+  dynamic var currentSidebarItems = [CPHomeWindowDocumentEntry]()
+
+
+  @IBOutlet weak var recentButton: NSButton!
+  @IBOutlet weak var spotlightButton: NSButton!
+
+  var buttons :[NSButton] { return [recentButton, spotlightButton] }
 
   @IBOutlet weak var spotlightSource: CPSpotlightPodfileController!
   @IBOutlet weak var recentSource: CPRecentDocumentsController!
 
-  @IBAction func recentButtonTapped(sender: AnyObject) {
+  @IBAction func recentButtonTapped(sender: NSButton) {
     currentSidebarItems = recentSource.recentDocuments
+    deselectButton(sender)
   }
 
-  @IBAction func spotlightButtonTapped(sender: AnyObject) {
+  @IBAction func spotlightButtonTapped(sender: NSButton) {
     currentSidebarItems = spotlightSource.documents
+    deselectButton(sender)
+  }
+
+  func deselectButton(button:NSButton) {
+    button.bordered = true
+    button.enabled = false
+
+    let otherButtons = buttons.filter { $0 != button }
+    otherButtons.forEach { (button) in
+      button.bordered = false
+      button.enabled = true
+    }
   }
 }
