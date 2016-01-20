c30cd064c5af5554987ec6a70e49467d2c74cb2b
Aaron Pearce <aaron@sunya.co.nz>
2016-01-20 18:35:14 +1300

    Fire open notification only if we have a document

--- app/CocoaPods/CPDocumentController.swift
+++ app/CocoaPods/CPDocumentController.swift
@@ -8,9 +8,15 @@ import Cocoa
   // All of the `openDocument...` calls end up calling this one method, so adding our notification here is simple
   
   override func openDocumentWithContentsOfURL(url: NSURL, display displayDocument: Bool, completionHandler: (NSDocument?, Bool, NSError?) -> Void) {
-    super.openDocumentWithContentsOfURL(url, display: displayDocument, completionHandler: completionHandler)
+    super.openDocumentWithContentsOfURL(url, display: displayDocument) { (document, displayDocument, error) -> Void in
+      if let _ = document { // Only fire the notification if we have a document
+        NSNotificationCenter.defaultCenter().postNotificationName(CPDocumentController.DocumentOpenedNotification, object: self)
+      }
+      
+      completionHandler(document, displayDocument, error)
+    }
 
-    NSNotificationCenter.defaultCenter().postNotificationName(CPDocumentController.DocumentOpenedNotification, object: self)
+    
   }
   
   // `noteNewRecentDocument` ends up calling to this method so we can just override this one method
