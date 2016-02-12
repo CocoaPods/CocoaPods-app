69f49df9cebbbc74307fd675387c170ce577842a
Nate West <nwestr@gmail.com>
2016-02-12 12:58:39 -0500

    Start creating test friendly app delegate

--- app/CocoaPods/Supporting Files/main.m
+++ app/CocoaPods/Supporting Files/main.m
@@ -1,5 +1,24 @@
 #import <Cocoa/Cocoa.h>
+#import "CocoaPods-Swift.h"
+
+int CPApplicationMain(int argc, const char *argv[]) {
+  [NSApplication sharedApplication];
+
+  id<NSApplicationDelegate> testDelegate = nil;
+  if (NSClassFromString(@"XCTestCase") != Nil) {
+    NSString *testBundlePath = [[NSProcessInfo processInfo] environment][@"XCInjectBundle"];
+    NSCParameterAssert(testBundlePath);
+    NSCParameterAssert([[NSBundle bundleWithPath:testBundlePath] load]);
+    testDelegate = [NSClassFromString(@"CPTestHelper") new];
+    NSApp.delegate = testDelegate;
+  }
+
+  [NSBundle loadNibNamed:@"MainMenu" owner:NSApp];
+
+  [NSApp run];
+  return 0;
+}
 
 int main(int argc, const char * argv[]) {
-  return NSApplicationMain(argc, argv);
+  return CPApplicationMain(argc, argv);
 }
