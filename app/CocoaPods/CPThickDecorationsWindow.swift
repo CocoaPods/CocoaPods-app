4b517af72c05ef6f991418af8983323c60ffdf6e
Pedro Vieira <pedrovcristao@hotmail.com>
2016-05-29 04:05:57 +0100

    Remove duplicated notification and code cleanup

--- app/CocoaPods/CPThickDecorationsWindow.swift
+++ app/CocoaPods/CPThickDecorationsWindow.swift
@@ -1,31 +1,30 @@
 import Cocoa
 
-/// Thick banded window - used in then Home window
+/// Thick banded window - used in the Home window
 
 class CPThickDecorationsWindow: NSWindow {
 
   /// We want to offset the window buttons to make them centered.
 
   override func makeKeyAndOrderFront(sender: AnyObject?) {
-
     titlebarAppearsTransparent = true
     movableByWindowBackground = true
     titleVisibility = .Hidden
 
     super.makeKeyAndOrderFront(sender)
 
-    [NSWindowDidResizeNotification, NSWindowDidResizeNotification, NSWindowDidMoveNotification].forEach { notification in
+    [NSWindowDidResizeNotification, NSWindowDidMoveNotification].forEach { notification in
       NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(moveWindowButtons), name: notification, object: self)
     }
     moveWindowButtons()
   }
 
   func moveWindowButtons(){
-    let verticalOffset = 12
+    let verticalOffset: CGFloat = 12
 
-    [NSWindowButton.CloseButton, NSWindowButton.MiniaturizeButton, NSWindowButton.ZoomButton].forEach { type in
+    ([.CloseButton, .MiniaturizeButton, .ZoomButton] as [NSWindowButton]).forEach { type in
       guard let button = standardWindowButton(type) else { return }
-      button.setFrameOrigin(NSMakePoint(button.frame.origin.x, CGFloat(verticalOffset)))
+      button.setFrameOrigin(NSMakePoint(button.frame.origin.x, verticalOffset))
     }
   }
 }
